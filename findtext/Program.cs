3cabe7b7ad85e8bc9d99db60852b2ee33da9feb8
A1exCart3r <alexkyprpro@gmail.com>
2024-02-05 19:07:16 +0300

    feature: findtext v1 added

---
+++ findtext/Program.cs
@@ -0,0 +1,47 @@
+﻿internal class Program
+{
+    private static int Main(string[] args)
+    {
+        if (args.Length != 2)
+        {
+            Console.WriteLine("Неверное количество аргументов - Введите аргументы в формате \"findtext.exe \"Евгений Онегин.txt\" \"Я к Вам пишу\"\"");
+            return 1;
+        }
+
+        string file = args[0];
+        string lineFromFile;
+        string searchText = args[1];
+        int lineNumber = 1;
+        bool textIsFound = false;
+
+        if (searchText == "") 
+        {
+            Console.WriteLine("Вы ничего не ввели для поиска");
+            return 1;
+        }
+
+        try
+        {
+            using(StreamReader sr = new StreamReader(file))
+            {
+                while (!sr.EndOfStream)
+                {
+                    lineFromFile = sr.ReadLine();
+                    if (lineFromFile.Contains(searchText))
+                    {
+                        Console.WriteLine(lineNumber);
+                        textIsFound = true;
+                    }
+                    lineNumber++;
+                }
+            }
+            if (!textIsFound) Console.WriteLine("Text Not Found");
+            return 0; //возращаю 0 а не 1 для корректности, ибо программа работает исправно
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Ошибка");
+            return 1;
+        }
+    }
+}
