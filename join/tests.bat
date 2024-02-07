2bd0f2e756f4e96701e9f392dbb2ee33844e9f18
A1exCart3r <alexkyprpro@gmail.com>
2024-02-08 01:00:10 +0300

    feature: added join
    https://github.com/alexey-malov/oop/tree/master/tasks/01#%D0%B2%D0%B0%D1%80%D0%B8%D0%B0%D0%BD%D1%82-5--join--50-%D0%B1%D0%B0%D0%BB%D0%BB%D0%BE%D0%B2-

---
+++ join/tests.bat
@@ -0,0 +1,17 @@
+chcp 65001
+
+REM Обычное слияние нескольких файлов
+join.exe "file1.dat" "file2.dat" "file3.dat" "output.dat" || goto err
+echo Тест 1 пройден
+
+REM Указание несуществующих файлов приведет к ошибке
+join.exe "missingfile1.dat" "file2.dat" "missingfile3.dat" "output.dat" && goto err
+echo Тест 2 пройден
+
+REM Недостаточное количество аргументов выведет инструкцию
+join.exe "file1.dat" "output.dat" && goto err
+echo Тест 3 пройден
+
+:err
+echo Тесты не пройдены
+exit /b 1
