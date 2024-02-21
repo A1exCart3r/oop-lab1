b756604364a9d286d2df9cd3cb3a96c14eb234e7
A1exCart3r <alexkyprpro@gmail.com>
2024-02-22 00:26:13 +0300

    update to tests.bat
    more tests

--- extract/tests.bat
+++ extract/tests.bat
@@ -1,4 +1,4 @@
-﻿chcp 65001
+chcp 65001
 
 REM Обычное извлечение фрагмента
 extract.exe "file1.dat" "file2.dat" 2 12 || goto err
@@ -27,6 +27,14 @@ echo Тест 6 пройден
 REM Некорекктное имя выходного файла - ошибка
 extract.exe "file1.dat" "file?2.dat" 2 0 && goto err
 echo Тест 7 пройден
+
+REM Неверное количество аргументов вызовет гайд
+extract.exe "file1.dat" "file2.dat" 2 && goto err
+echo Тест 8 пройден
+
+REM Неправильно введеные аргументы приведут к ошибке
+extract.exe "file1.dat" "file2.dat" 2 ? && goto err
+echo Тест 9 пройден
 
 REM Тесты пройдены
 echo Тесты пройдены
